881eff4f64ae07443d8f581e5a5c49d0d4d0b354
CristianOlivera1 <221181@unamba.edu.pe>
2024-06-29 09:18:24 -0500

    first commit

---
+++ BaseDatosMySQL/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace BaseDatosMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTabla = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnCrear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTabla
+            // 
+            this.dgvTabla.BackgroundColor = System.Drawing.Color.DarkCyan;
+            this.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabla.Location = new System.Drawing.Point(15, 72);
+            this.dgvTabla.Name = "dgvTabla";
+            this.dgvTabla.Size = new System.Drawing.Size(592, 264);
+            this.dgvTabla.TabIndex = 0;
+            this.dgvTabla.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTabla_CellClick);
+            this.dgvTabla.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Cyan;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 34);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(119, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(639, 216);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(119, 33);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Cyan;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DNI";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Cyan;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(292, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 19);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(431, 34);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(160, 20);
+            this.txtApellido.TabIndex = 11;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(296, 34);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(106, 20);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(159, 34);
+            this.txtDni.MaxLength = 8;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(116, 20);
+            this.txtDni.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Cyan;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(593, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Fecha de Nacimiento";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Cyan;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(427, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 19);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Apellidos";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(639, 165);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(119, 36);
+            this.btnActualizar.TabIndex = 16;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRegistrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistrar.Location = new System.Drawing.Point(639, 113);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(119, 37);
+            this.btnRegistrar.TabIndex = 17;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(597, 34);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 18;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(639, 264);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(119, 31);
+            this.btnCrear.TabIndex = 19;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTabla);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.MediumBlue;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTabla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
---
+++ BaseDatosMySQL/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BaseDatosMySQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        } // Establecer la conexión a la base de datos
+        static string conexion = "server=localhost;port=3306;database=dbstorage;pwd='';uid=root;";
+        MySqlConnection conectado = new MySqlConnection(conexion);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvTabla.DataSource = llenar();
+        }
+        public DataTable llenar()
+        {
+            conectado.Open();
+            DataTable dt = new DataTable();
+            string llenar = "select * from tperson";
+            MySqlCommand cmd = new MySqlCommand(llenar, conectado);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conectado.Close();
+            return dt;
+        }
+        //REGISTRAR
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Expresiones regulares para validación
+            string dniPattern = @"^\d{8}$"; // DNI debe tener 8 dígitos
+            string namePattern = @"^[a-zA-Zñáéíóú]+(?: [a-zA-Zñáéíóú]+)*$";  // Nombres y apellidos deben contener solo letras
+
+            // Validar campos
+            if (!Regex.IsMatch(txtDni.Text, dniPattern))
+            {
+                MessageBox.Show("DNI no válido. Debe contener 8 dígitos sin caracteres especiales.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Regex.IsMatch(txtNombre.Text, namePattern))
+            {
+                MessageBox.Show("Nombre no válido. Debe contener solo letras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Regex.IsMatch(txtApellido.Text, namePattern))
+            {
+                MessageBox.Show("Apellido no válido. Debe contener solo letras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try { 
+                    conectado.Open();
+
+                    // Verificar si el ID ya existe
+                    string verificar = "SELECT COUNT(1) FROM tperson WHERE idPerson = @idPerson";
+                    using (MySqlCommand cmdVerificar = new MySqlCommand(verificar, conectado))
+                    {
+                        cmdVerificar.Parameters.AddWithValue("@idPerson", txtId.Text);
+                        int count = Convert.ToInt32(cmdVerificar.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("El ID ya existe. No se puede insertar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            conectado.Close();
+                            return;
+                        }
+                    }
+
+                // Insertar un nuevo registro
+                string registrar = "INSERT INTO tperson (idPerson, dni, firstName, surName, birthDate) " +
+                                   "VALUES (@idPerson, @dni, @firstName, @surName, @birthDate)";
+                    using (MySqlCommand cmd = new MySqlCommand(registrar, conectado))
+                    {
+                        cmd.Parameters.AddWithValue("@idPerson", txtId.Text);
+                        cmd.Parameters.AddWithValue("@dni", txtDni.Text);
+                        cmd.Parameters.AddWithValue("@firstName", txtNombre.Text);
+                        cmd.Parameters.AddWithValue("@surName", txtApellido.Text);
+                        cmd.Parameters.AddWithValue("@birthDate", dtpFecha.Value);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    conectado.Close();
+                    dgvTabla.DataSource = llenar();
+                    MessageBox.Show("Los Datos fueron Registrados correctamente");
+            }catch (MySqlException ex)
+            {
+                if (ex.Number == 1062) // Código de error para duplicado de clave única
+                {
+                    MessageBox.Show("El ID ya existe. No se puede insertar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrió un error al insertar el registro: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al insertar el registro: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (conectado.State == ConnectionState.Open)
+                {
+                    conectado.Close();
+                }
+            }
+        }
+        //ACTUALIZAR
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            // Expresiones regulares para validación
+            string dniPattern = @"^\d{8}$"; // DNI debe tener 8 dígitos
+            string namePattern = @"^[a-zA-Zñáéíóú]+(?: [a-zA-Zñáéíóú]+)*$";  // Nombres y apellidos deben contener solo letras
+
+            // Validar campos
+            if (!Regex.IsMatch(txtDni.Text, dniPattern))
+            {
+                MessageBox.Show("DNI no válido. Debe contener 8 dígitos sin caracteres especiales.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Regex.IsMatch(txtNombre.Text, namePattern))
+            {
+                MessageBox.Show("Nombre no válido. Debe contener solo letras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Regex.IsMatch(txtApellido.Text, namePattern))
+            {
+                MessageBox.Show("Apellido no válido. Debe contener solo letras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            conectado.Open();
+            // actualizar un registro
+            string actualizar = "update tperson set dni=@dni, firstName = @firstName,surName=@surName,birthDate=@birthDate where idPerson=@idPerson";
+            using (MySqlCommand cmd = new MySqlCommand(actualizar, conectado))
+            {
+                cmd.Parameters.AddWithValue("@idPerson", txtId.Text);
+                cmd.Parameters.AddWithValue("@dni", txtDni.Text);
+                cmd.Parameters.AddWithValue("@firstName", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@surName", txtApellido.Text);
+                cmd.Parameters.AddWithValue("@birthDate", dtpFecha.Value);
+
+                cmd.ExecuteNonQuery();
+                conectado.Close();
+
+                dgvTabla.DataSource = llenar();
+                MessageBox.Show("Su registro fue realizado correctamente.", "Registro Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+             {
+                 txtId.Text = dgvTabla.CurrentRow.Cells[0].Value.ToString();
+                 txtDni.Text = dgvTabla.CurrentRow.Cells[1].Value.ToString();
+                 txtNombre.Text = dgvTabla.CurrentRow.Cells[2].Value.ToString();
+                 txtApellido.Text = dgvTabla.CurrentRow.Cells[3].Value.ToString();
+                 dtpFecha.Text = dgvTabla.CurrentRow.Cells[4].Value.ToString();
+             }
+             catch
+             {
+                MessageBox.Show("Error ");
+
+             }
+
+        }
+        //ELIMINAR
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            conectado.Open(); // eliminar un registro
+            string eliminar = "DELETE FROM tperson WHERE idPerson= @idPerson";
+            using (MySqlCommand cmd = new MySqlCommand(eliminar, conectado))
+            {
+                cmd.Parameters.AddWithValue("@idPerson", txtId.Text);
+
+                cmd.ExecuteNonQuery();
+
+                conectado.Close();
+
+                dgvTabla.DataSource = llenar();
+                MessageBox.Show("Datos eliminados Correctamente");
+            }
+        }
+        //CREAR
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            txtId.Clear();
+            txtDni.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();    
+        }
+
+        private void dgvTabla_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtId.Text = dgvTabla.CurrentRow.Cells[0].Value.ToString();
+                txtDni.Text = dgvTabla.CurrentRow.Cells[1].Value.ToString();
+                txtNombre.Text = dgvTabla.CurrentRow.Cells[2].Value.ToString();
+                txtApellido.Text = dgvTabla.CurrentRow.Cells[3].Value.ToString();
+                dtpFecha.Text = dgvTabla.CurrentRow.Cells[4].Value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Error ");
+
+            }
+
+
+        }
+    }
+}
